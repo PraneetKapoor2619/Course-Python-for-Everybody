d1c08bb02e060d5f4a55b0616c87cdb506fab019
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2020-05-24 22:38:48 +0530

    experiment with for loops

---
+++ exp2.py
@@ -0,0 +1,13 @@
+print("LOLSY")
+for n in range(1, 101):
+    if n%2 != 0 :
+        if (n == 3) or (n == 5) :
+            if n == 3 :
+                print("Now 3 is a prime number!!")
+            if n == 5 :
+                print("FIve is dad")
+        else:
+            print("Ok, ODD!!!")
+    else:
+        print(str(n) + ". Suck it, sucker!!")
+print("Good sucking!!")
