692687bd083d4328aaf5c8513602d0d98173a5f9
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2020-05-24 16:41:20 +0530

    experiment with conditional stat.

---
+++ exp1.py
@@ -0,0 +1,15 @@
+x = 5
+print('Before 5')
+if x == 5:
+    print('Is 5')
+    
+    print('Is Still 5')
+    #lolsy
+    print('Third 5')
+print('Afterwards 5')
+print('Before 6')
+if x == 6:
+    print('is 6')
+    print('is still 6')
+    print('Third 6')
+print('Afterwards 6')
