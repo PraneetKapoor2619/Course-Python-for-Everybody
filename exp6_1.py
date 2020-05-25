f927bca791dd785b2290ba43c016553e78037031
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2020-05-25 18:50:24 +0530

    a small code using try-except

---
+++ exp6_1.py
@@ -0,0 +1,6 @@
+try :
+    number = int(input("Enter a number: "))
+except :
+    print("That is not a number!! One more chance idiot!!")
+    number = int(input("Enter a number: "))
+print("That's it!!")
