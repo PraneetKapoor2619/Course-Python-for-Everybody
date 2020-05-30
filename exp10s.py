3c4b7597569e8101ef387390e7dfa8f6f2fd1890
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2020-05-30 13:14:30 +0530

    First code in emacs

---
+++ exp10s.py
@@ -0,0 +1,5 @@
+import os
+os.system("CLS")
+for number in range(1, 101) :
+    print(str(number)+". Hello, World!! I am using Emacs!!")
+print("Haha!! I am using emacs!!!\n I want to make this program special.")
